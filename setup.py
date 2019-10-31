77e2054855e60dedb8e35453dcc268685e82e7da
yochai <yochail@gmail.com>
2019-10-31 15:47:15 +0200

    master

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='CalendarEventsServer',
+	version='',
+	packages=[''],
+	url='',
+	license='',
+	author='Yochai',
+	author_email='',
+	description=''
+)
+
